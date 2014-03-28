d3466bbeaca9c5944b14ac62cd4b6c1bed12cf81
Youk Chang SU <csyouk@hanmail.net>
2014-03-28 20:58:18 +0900

    화면을 분할해 보았다.

---
+++ Python/squareRootBi.py
@@ -0,0 +1,21 @@
+def squareRootBi(x, epsilon):
+    """Assume x >= 0 and epsilon > 0
+       Return y s.t. y*y is within epsilon of x"""
+    assert x >= 0, 'x must be not non-negative, not' + str(x)
+    assert epsilon > 0, 'epsilon must be positive, not' + str(epsilon)
+    low = 0
+    high = x
+    guess = (low + high)/2.0
+    ctr = 1
+    while abs(guess**2 - x) > epsilon and ctr <= 100:
+        #print 'low:', low, 'high:', high, 'guess:', guess
+        if guess**2 < x:
+            low = guess
+
+        else:
+            high = guess
+        guess = ( low + high )/2.0
+        ctr += 1
+    assert ctr <= 100, 'Iteration count exceeded'
+    print 'Bisection method. Num. Oterations:' , ctr, 'Estimate:', guess
+    return guess
