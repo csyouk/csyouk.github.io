337dc43d4aebe82b1ef13c92a6b005dfde1ba0da
Youk Chang SU <csyouk@hanmail.net>
2016-04-06 02:01:34 +0900

    change effect

--- custom/js/custom.js
+++ custom/js/custom.js
@@ -8,4 +8,4 @@ console.log("app init done!");
 new Taggle('post1', {
     tags: ['d3']
 });
-$(".post1").airport(["D3", "Data", "Visualization"],{fill_space: true, transition_speed:1000, loop:false});
+$(".post1").airport(["Data", "Visualization", "With D3.js"],{fill_space: true, transition_speed:1000, loop:true});
