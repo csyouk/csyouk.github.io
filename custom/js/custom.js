5c1acb4a4fcf9d9c40da4cd1586dd27b75bc64ab
Youk Chang SU <csyouk@hanmail.net>
2016-04-06 01:59:23 +0900

    add multiple lib

--- custom/js/custom.js
+++ custom/js/custom.js
@@ -4,3 +4,8 @@ function change(fileName) {
   window.location.replace(location);
 }
 console.log("app init done!");
+
+new Taggle('post1', {
+    tags: ['d3']
+});
+$(".post1").airport(["D3", "Data", "Visualization"],{fill_space: true, transition_speed:1000, loop:false});
