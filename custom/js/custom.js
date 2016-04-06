795492f0f3cebd72f44ba64fdc96a711db19e059
Youk Chang SU <csyouk@hanmail.net>
2016-04-07 01:12:00 +0900

    update java generic document

--- custom/js/custom.js
+++ custom/js/custom.js
@@ -3,9 +3,16 @@ function change(fileName) {
   location = fileName + '.html'
   window.location.replace(location);
 }
+function getRandomInt(min, max){
+  return Math.floor(Math.random()*(max-min)) + min;
+}
 console.log("app init done!");
 
 new Taggle('post1', {
     tags: ['d3']
 });
-$(".post1").airport(["Data", "Visualization", "With D3.js"],{fill_space: true, transition_speed:1000, loop:true});
+new Taggle('post2', {
+    tags: ['java','OOP']
+});
+$(".post1").airport(["Data", "Visualization", "With D3.js"],{fill_space: true, transition_speed:getRandomInt(1000,3000), loop:true});
+$(".post2").airport(["Java", "Generic"],{fill_space: true, transition_speed:getRandomInt(1000,3000), loop:true});
